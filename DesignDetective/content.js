d8906fcd807c9363b5ba12bcb9cf1e2bd1789284
w0nam <lucaslefevre11@gmail.com>
2024-12-25 19:36:30 +0100

    don't know

--- DesignDetective/content.js
+++ DesignDetective/content.js
@@ -1,25 +1,6 @@
-/*
-Will not use AI for now: don't have any time available to train a model:
-i'll create event listener and user librairies and function to analyse & return
-font & color palette of a given website, after the DOM is loaded and the user
-requested it.
-const axios = require('axios/dist/browser/axios.cjs');
-*/
+'use strict';
 
-let btn1 = document.getElementById("btn-1")
-
-document.addEventListener("click", function() {
-  console.log('button 1 clicked!');
+let btn = document.getElementById('btn-1')
+document.addEventListener("click", () => {
+  alert("you clicked a button");
 });
-
-
-
-
-//async function returnFont() { 
-//}
-
-//async function returnColors() {
-//}
-
-//async function returnAll() {
-//}
