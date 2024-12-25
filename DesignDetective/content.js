d2502826c1d28bd2ece98a42e7e534fe7fc8e084
w0nam <lucaslefevre11@gmail.com>
2024-12-25 17:51:57 +0100

    Working on defining and properly differenciating button from my popup.html in my content.js.

--- DesignDetective/content.js
+++ DesignDetective/content.js
@@ -3,12 +3,17 @@ Will not use AI for now: don't have any time available to train a model:
 i'll create event listener and user librairies and function to analyse & return
 font & color palette of a given website, after the DOM is loaded and the user
 requested it.
-*/
 const axios = require('axios/dist/browser/axios.cjs');
+*/
 
-document.addEventListener("DOMContentLoaded", () => {
-  alert("Content loaded");
+let btn1 = document.getElementById("btn-1")
+
+document.addEventListener("click", function() {
+  console.log('button 1 clicked!');
 });
+
+
+
 
 //async function returnFont() { 
 //}
