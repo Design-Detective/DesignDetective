dc8218bf282557524c933425cf2f3be3e95f5a5f
w0nam <lucaslefevre11@gmail.com>
2024-12-25 17:23:21 +0100

    Completement restructurer les différents fichier: Je ne peu pas utiliser l'IA à cause de problème de propriété intellectuel et de tarification.

--- DesignDetective/content.js
+++ DesignDetective/content.js
@@ -1,23 +1,19 @@
-import * as tf from '@tensorflow/tfjs';
+// Will not use AI for now: don't have any time available to train a model:
+// i'll create event listener and user librairies and function to analyse & return
+// font & color palette of a given website, after the DOM is loaded and the user
+// requested it.
 
+const axios = require('axios/dist/browser/axios.cjs');
 
-// Cette route, de train monpropre modèle, me paraît trop longue et trop compliqué pour mes besoin.
-// Je vais sûrement importer et run un modèle déjà entraîner de Github. On verras bien.
-// Use `tfjs`.
-const tf = require('@tensorflow/tfjs');
-// Define a simple model.
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}));
-model.add(tf.layers.dense({units: 1, activation: 'linear'}));
-model.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
-
-const xs = tf.randomNormal([100, 10]);
-const ys = tf.randomNormal([100, 1]);
-// Train the model.
-model.fit(xs, ys, {
-  epochs: 100,
-  callbacks: {
-    onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss = ${log.loss}`)
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  alert("Content loaded");
 });
 
+//async function returnFont() { 
+//}
+
+//async function returnColors() {
+//}
+
+//async function returnAll() {
+//}
