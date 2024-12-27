508e8f24d3ed28de54c1ef11cdd0d08db486cd99
w0nam <lucaslefevre11@gmail.com>
2024-12-27 13:45:12 +0100

    WiP

--- DesignDetective/scripts/contentScript.js
+++ DesignDetective/scripts/contentScript.js
@@ -8,20 +8,35 @@ chrome.runtime.onInstalled.addListener(({ reason }) => {
   }
 });
 
+// Set some UI element when using the extension:
 chrome.action.setBadgeText({ 
   text: "ON" 
 });
-
 chrome.action.setBadgeBackgroundColor({
   color: "#5A3737"
 });
 
+const btn = document.getElementById("btn");
 // add event listener for "on click":
-chrome.action.onClicked.addListener((tab) => {
-  async function getCurrentTab() {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    // `tab` will either be a `tabs.Tab` instance or `undefined`.
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return  tab;
+btn.addEventListener('click', async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  try {
+    // `tab` will either be a `tabs.Tab` instance or an empty array (`[]`).
+    const tab = await chrome.tabs.query(queryOptions);
+    if (tab.length > 0) {
+      const htmlDoc = new DOMParser().parseFromString(tab.content, 'text/html');
+      const styleElements = htmlDoc.querySelectorAll('[style]');
+      
+      if (styleElements.length > 0) {
+
+      }
+    } 
+    else {
+      alert("Open a tab you want to analyse first.");
+    }
+  } 
+  catch (error) {
+    console.error('Error querying tabs:', error);
   }
 });
+
