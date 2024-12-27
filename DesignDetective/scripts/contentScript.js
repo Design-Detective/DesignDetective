778319564e7b2388695cb6487b07249663e37cec
w0nam <lucaslefevre11@gmail.com>
2024-12-27 12:30:02 +0100

    WiP

--- DesignDetective/scripts/contentScript.js
+++ DesignDetective/scripts/contentScript.js
@@ -1,6 +1,27 @@
-// to retrieve the url's css.
-const btn = document.getElementById("btn");
-// little test first
-btn.onclick = function findStyleSheet(activeInfo) {
-  console.log(activeInfo);
-}
+// file that serve to subscribe the background worker of my web extension.
+// thanks.html when installing the extension:
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+  if (reason === "install") {
+    chrome.tabs.create({
+      url: "thanks.html",
+    });
+  }
+});
+
+chrome.action.setBadgeText({ 
+  text: "ON" 
+});
+
+chrome.action.setBadgeBackgroundColor({
+  color: "#5A3737"
+});
+
+// add event listener for "on click":
+chrome.action.onClicked.addListener((tab) => {
+  async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return  tab;
+  }
+});
